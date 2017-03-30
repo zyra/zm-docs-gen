461759cc3f129112d28f9d21eb0cca1c225b3b34
Ibby Hadeed <ibby93@gmail.com>
2017-03-30 18:01:44 -0400

    fix config path

--- src/zm-docs-gen.ts
+++ src/zm-docs-gen.ts
@@ -1,5 +1,5 @@
 try {
-    const [Dgeni, config] = [require('dgeni'), require('../../../docs.config.js')];
+    const [Dgeni, config] = [require('dgeni'), require('../../../../docs.config.js')];
     new Dgeni(config.dgeniConfig)
         .generate()
         .then((docs: any) => console.log(`${docs.length} docs generated.`));
