34e77f5c5c12ea77863b679c8a07460a94e927e0
Ibby Hadeed <ibby93@gmail.com>
2017-04-01 06:52:25 -0400

    fix the hidden stuff

--- src/processors/remove-hidden.ts
+++ src/processors/remove-hidden.ts
@@ -1,18 +1,22 @@
+function shouldShow(doc: any) {
+  if (doc.tags.tagsByName.get('hidden')) {
+    return false;
+  }
+  doc.members = doc.members.filter((member: any) => !member.tags.tagsByName.get('hidden'));
+  doc.statics = doc.statics.filter((method: any) => !method.tags.tagsByName.get('hidden'));
+  return true;
+}
+
 module.exports = {
   name: 'remove-hidden',
   description: 'Removes anything with @hidden tag',
   $runBefore: ['rendering-docs'],
   $process: (docs: any[]) => docs.filter((doc: any) => {
-
-    doc.moduleDoc.exports = doc.moduleDoc.exports.filter((doc: any) => {
-      if (doc.tags.tagsByName.get('hidden')) {
-        return false;
-      }
-      doc.members = doc.members.filter((member: any) => !member.tags.tagsByName.get('hidden'));
-      doc.statics = doc.statics.filter((method: any) => !method.tags.tagsByName.get('hidden'));
+    if (shouldShow(doc)) {
+      doc.moduleDoc.exports = doc.moduleDoc.exports.filter((doc: any) => shouldShow(doc));
       return true;
-    });
-
-    return true;
+    } else {
+      return false;
+    }
   })
 };
