6a537be018dc848e8819a0e1932dff53df446ee3
Ibby Hadeed <ibby93@gmail.com>
2017-04-01 07:22:12 -0400

    fixes

--- src/processors/remove-hidden.ts
+++ src/processors/remove-hidden.ts
@@ -1,10 +1,13 @@
-function shouldShow(doc: any) {
-  if (!doc.tags || !!doc.tags.tagsByName.get('hidden')) {
-    return false;
-  }
+import * as _ from 'lodash';
+
+function filterMembers(doc: any) {
   doc.members = doc.members.filter((member: any) => !member.tags.tagsByName.get('hidden'));
   doc.statics = doc.statics.filter((method: any) => !method.tags.tagsByName.get('hidden'));
-  return true;
+  return doc;
+}
+
+function shouldShow(doc: any) {
+  return !doc.tags || !!doc.tags.tagsByName.get('hidden');
 }
 
 module.exports = {
@@ -12,12 +15,15 @@ module.exports = {
   description: 'Removes anything with @hidden tag',
   $runAfter: ['tags-parsed'],
   $runBefore: ['rendering-docs'],
-  $process: (docs: any[]) => docs.filter((doc: any) => {
-    if (shouldShow(doc)) {
-      doc.moduleDoc.exports = doc.moduleDoc.exports.filter((doc: any) => shouldShow(doc));
-      return true;
-    } else {
-      return false;
-    }
-  })
+  $process: (docs: any[]) => {
+    return _(docs).filter(shouldShow).map(filterMembers).valueOf();
+    // docs.filter((doc: any) => {
+    //   if (shouldShow(doc)) {
+    //     doc.moduleDoc.exports = doc.moduleDoc.exports.filter((doc: any) => shouldShow(doc));
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // })
+  }
 };
